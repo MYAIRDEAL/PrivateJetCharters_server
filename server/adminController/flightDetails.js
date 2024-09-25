6940b3437401529b0ef2501198574318bf66e58e
Sanjay Kandula <sanjaykandula3@gmail.com>
2024-09-25 13:34:41 +0530

    flight details update

--- server/adminController/flightDetails.js
+++ server/adminController/flightDetails.js
@@ -11,3 +11,49 @@ exports.getAllPushData=async(req,res)=>{
         return res.status(500).json({message:"Internal server Error"})
     }
 }
+
+exports.addFlightDetails = async (req, res) => {
+  try {
+    const {
+      section,
+      departure,
+      arrival,
+      journeytype,
+      date,
+      fromtime,
+      endtime,
+      reachdate,
+      pax,
+      chartertype,
+    } = req.body;
+    if (
+      !section ||
+      !departure ||
+      !arrival ||
+      !journeytype ||
+      !date ||
+      !fromtime ||
+      !endtime ||
+      !reachdate ||
+      !pax ||
+      !chartertype
+    ) {
+      return res.status(404).json({ message: "Missing fileds" });
+    }
+    const flightData=new flightDetails({
+        section,
+        departure,
+        arrival,
+        journeytype,
+        date,
+        fromtime,
+        endtime,
+        reachdate,
+        pax,
+        chartertype,
+    })
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server Error" });
+  }
+};
